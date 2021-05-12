ea5d09a157447d467110c1d87361e16f846c7071
winton <mail@wintoni.us>
2021-05-12 13:28:30 -0700

    Check that return type is object before merging

--- src/importRunner.ts
+++ src/importRunner.ts
@@ -15,8 +15,8 @@ export async function importRunner(
     const { default: fn } = await promise
 
     if (fn) {
-      const out = await fn(memo)
-      Object.assign(memo, out)
+      const obj = await fn(memo)
+      addObjectToMemo({ memo, obj })
     }
   } else if (all) {
     const out = await Promise.all(
@@ -25,25 +25,37 @@ export async function importRunner(
       )
     )
     for (const obj of out) {
-      Object.assign(memo, obj)
+      addObjectToMemo({ memo, obj })
     }
   } else if (each) {
     for (const input of each) {
-      const out = await importRunner(
+      const obj = await importRunner(
         addMemoToInput({ memo, input })
       )
-      Object.assign(memo, out)
+      addObjectToMemo({ memo, obj })
     }
   }
 
   return memo
 }
 
+export function addObjectToMemo({
+  memo,
+  obj,
+}: {
+  memo: any
+  obj: any
+}): void {
+  if (typeof obj === "object" && !Array.isArray(obj)) {
+    Object.assign(memo, obj)
+  }
+}
+
 export function addMemoToInput({
   memo,
   input,
 }: {
-  memo: Record<string, any>
+  memo: any
   input: ImportRunnerInput | Promise<ImportType>
 }): ImportRunnerInput {
   return input["then"]
--- src/importRunnerTypes.ts
+++ src/importRunnerTypes.ts
@@ -1,10 +1,10 @@
 export interface ImportType {
-  default: (arg: Record<string, any>) => Record<string, any>
+  default: (arg: any) => Promise<any>
 }
 
 export interface ImportRunnerInput {
   all?: (ImportRunnerInput | Promise<ImportType>)[]
   each?: (ImportRunnerInput | Promise<ImportType>)[]
-  memo?: Record<string, any>
+  memo?: any
   promise?: Promise<ImportType>
 }
